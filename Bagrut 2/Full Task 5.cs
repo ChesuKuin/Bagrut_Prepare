9108bf6569536dc5df7ea526e6a3b5bed71f71fe
Daria <66951217+ChesuKuin@users.noreply.github.com>
2024-09-03 18:02:38 +0300

    Full Task 5.cs

---
+++ Bagrut 2/Full Task 5.cs
@@ -0,0 +1,97 @@
+using System;
+
+public class Lesson
+{
+    private int id;
+    private int hh;
+    private int mm;
+    private int duration;
+
+    public Lesson(int id, int hh, int mm, int duration)
+    {
+        this.id = id;
+        this.hh = hh;
+        this.mm = mm;
+        this.duration = duration;
+    }
+
+    public int GetId() => id;
+    public int GetHH() => hh;
+    public int GetMM() => mm;
+    public int GetDuration() => duration;
+
+    public bool IsLater(Lesson other)
+    {
+        if (this.hh > other.hh) return true;
+        if (this.hh == other.hh && this.mm > other.mm) return true;
+        return false;
+    }
+
+    public static int Last(Lesson[] a)
+    {
+        Lesson lastLesson = a[0];
+
+        for (int i = 1; i < a.Length; i++)
+        {
+            if (a[i].IsLater(lastLesson))
+            {
+                lastLesson = a[i];
+            }
+        }
+
+        return lastLesson.GetId();
+    }
+
+    public static int SumDuration(Lesson[] arr, int id)
+    {
+        int sum = 0;
+
+        foreach (Lesson lesson in arr)
+        {
+            if (lesson.GetId() == id)
+            {
+                sum += lesson.GetDuration();
+            }
+        }
+
+        return sum;
+    }
+
+    public static int Longest(Lesson[] arr)
+    {
+        int maxDuration = 0;
+        int maxId = -1;
+
+        foreach (Lesson lesson in arr)
+        {
+            int currentId = lesson.GetId();
+            int currentDuration = SumDuration(arr, currentId);
+
+            if (currentDuration > maxDuration)
+            {
+                maxDuration = currentDuration;
+                maxId = currentId;
+            }
+        }
+
+        return maxId;
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Lesson[] lessons = new Lesson[]
+        {
+            new Lesson(1, 8, 30, 45),
+            new Lesson(2, 9, 15, 30),
+            new Lesson(1, 10, 0, 60),
+            new Lesson(3, 11, 0, 90)
+        };
+
+        Console.WriteLine("Last lesson id: " + Lesson.Last(lessons));
+        Console.WriteLine("Total duration for id 1: " + Lesson.SumDuration(lessons, 1));
+        Console.WriteLine("Id with longest total duration: " + Lesson.Longest(lessons));
+    }
+}
