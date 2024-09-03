22a27a6ae98c1b5cfa8a03dedd8fdfcec1aaec2a
Daria <66951217+ChesuKuin@users.noreply.github.com>
2024-09-03 17:53:35 +0300

    Task 4, bet 1.cs

---
+++ Bagrut 2/Task 4, bet 1.cs
@@ -0,0 +1,62 @@
+using System;
+
+public class TourPackage
+{
+    public int Id { get; private set; }
+    public int Price { get; private set; }
+    public int MaxTime { get; private set; }
+    public int MaxData { get; private set; }
+    public int Extra { get; private set; }
+
+    public TourPackage(int id, int price, int maxTime, int maxData)
+    {
+        Id = id;
+        Price = price;
+        MaxTime = maxTime;
+        MaxData = maxData;
+        Extra = 0;
+    }
+
+    public void SetExtra(int minutes, int usage)
+    {
+        Extra = 0;
+        if (minutes > MaxTime)
+        {
+            Extra += (minutes - MaxTime) * 1;
+        }
+        if (usage > MaxData)
+        {
+            Extra += (usage - MaxData) * 2;
+        }
+    }
+
+    public static int Calculate(TourPackage[] packages)
+    {
+        int count = 0;
+        foreach (var package in packages)
+        {
+            if (package.Extra > 0)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void Main()
+    {
+        TourPackage[] packages = new TourPackage[]
+        {
+            new TourPackage(1, 100, 200, 10),
+            new TourPackage(2, 150, 100, 5),
+            new TourPackage(3, 200, 300, 20)
+        };
+
+        packages[0].SetExtra(250, 12); 
+        packages[1].SetExtra(90, 4); 
+        packages[2].SetExtra(350, 25); 
+
+        Console.WriteLine("Число клиентов с дополнительными платежами: " + Calculate(packages));
+    }
+}
+
